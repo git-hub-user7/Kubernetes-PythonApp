961461116a153789b09cfc16c837d5aa8e784ba3
DHWARAKESH SRINIVASAN <dhwarakesh99@gmail.com>
2025-03-08 00:15:03 +0530

    Update app.py

--- frontend/app.py
+++ frontend/app.py
@@ -1,13 +1,9 @@
 from flask import Flask
-import logging
-
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Welcome to my app!"
+    return "Welcome to My Flask App!"
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO)
-    app.logger.info("Starting Flask app...")
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    app.run(host='0.0.0.0', port=5000)
